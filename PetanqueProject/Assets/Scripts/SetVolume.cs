4a6de6425d7b81cb5410b90eb055bc046d31601e
MeneldiI <lu.correia71@gmail.com>
2020-02-29 00:01:19 +0100

    ajout d'un prototype de menu de démarrage
    ajout d'un prototype de menu de démarrage

---
+++ PetanqueProject/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+    public string ExposedVolume;
+
+    void Start()
+    {
+        slider.value = 0.75f;
+    }
+
+    public void SetLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat(ExposedVolume, Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat(ExposedVolume, sliderValue);
+    }
+}
